827fb8bd07fbcc37ea0d34ad1dca45c413e47125
vagrant <fcalvo@fsu.edu>
2016-04-28 21:31:26 +0000

    Created styles and responsiveness for main menu.

---
+++ js/fsutheme.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ */
+ 
+(function ($) {
+  Drupal.behaviors.fsutheme = {
+    attach: function(context, settings) {           
+      //Create drop down menu
+    }   
+  };
+})(jQuery);
