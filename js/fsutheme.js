ce63a65d2116197528586a7a549f35864b6f86ff
Favenzio Calvo <fcalvo@fsu.edu>
2021-07-13 07:53:29 -0400

    Moving to Primo changes for search.

--- js/fsutheme.js
+++ js/fsutheme.js
@@ -96,7 +96,7 @@
 function allSearch(search_form) {
   var sanitized_input = encodeURIComponent(document.forms[search_form]["all-search-input"].value);
   var query = document.forms[search_form]["base_query"].value;
-  query += "&bQuery=" + sanitized_input + "&cli0=FT&clv0=Y&cli1=FT1&clv1=Y&type=1";
+  query += sanitized_input + "&tab=Everything&search_scope=MyInst_and_CI&vid=01FALSC_FSU:Home&offset=0";
   window.location = query;
   return false;
 }
@@ -110,26 +110,31 @@ function articleSearch(search_form) {
   var peer_review_filter = document.getElementById("filter_peer_reviewed").checked;
   var date_range_filter = document.getElementById("filter_date_published").checked;
   var full_text_filter = document.getElementById("filter_article_full_text").checked;
+  var open_access_filter = document.getElementById("filter_open_access").checked;
 
   /* Create the query */
   var query = document.forms[search_form]["base_query"].value;
-  query += "&bQuery=" + encodeURIComponent(document.forms[search_form]["article_search_input"].value);
-  query += "&cli0=FT1&clv0=Y&type=1";
+  query += encodeURIComponent(document.forms[search_form]["article_search_input"].value);
+  query += "&tab=Everything&search_scope=MyInst_and_CI&vid=01FALSC_FSU:Home&facet=rtype,include,articles";
 
   if (full_text_filter) {
-    query += "&cli" + count + "=FT&clv" + count + "=Y";
-    count++;
+    query += "&mfacet=tlevel,include,online_resources,2";
   }
 
   if (peer_review_filter) {
-    query += "&cli" + count + "=RV&clv" + count + "=Y";
-    count++;
+    query += "&mfacet=tlevel,include,peer_reviewed,1";
+  }
+
+  if (open_access_filter) {
+    query += "&mfacet=tlevel,include,open_access,1";
   }
 
   if (date_range_filter) {
     var current_year = new Date().getFullYear();
-    query += "&cli" + count + "=DT1&clv" + count + "=" + document.forms[search_form]["date_range"].value + "01-" + current_year + "12"; 
+    query += "&facet=searchcreationdate,include," + document.forms[search_form]["date_range"].value + "%7C,%7C" + current_year;
   }
+
+  query += "&offset=0";
 
   window.location = query;
   return false;
@@ -139,26 +144,28 @@ function articleSearch(search_form) {
 function edsBookSearch(search_form) {
 
   /* Get Filters set by user */
-  var ebook_filter = document.getElementById("filter_book_full_text").checked;
-  var all_books_filter = document.getElementById("books_and_ebooks").checked;
+  var book_filter_element = document.getElementsByName("book_filter");
+  var book_filter = "all";
+
+  for(i = 0; i < book_filter_element.length; i++) {
+    if(book_filter_element[i].checked) {
+      book_filter = book_filter_element[i].value;
+    }
+  }
 
   /* Create the query */
   var query = document.forms[search_form]["base_query"].value;
-  query += "&bQuery=" + encodeURIComponent(document.forms[search_form]["book_search_input"].value) + "&cli0=FC&clv0=Y";
+  query += encodeURIComponent(document.forms[search_form]["book_search_input"].value) +
+    "&tab=Everything&search_scope=MyInst_and_CI&vid=01FALSC_FSU:Home&mfacet=rtype,include,books,1";
 
-  if (ebook_filter) {
-    query = "https://login.proxy.lib.fsu.edu/login?url=" +
-            "http://widgets.ebscohost.com/prod/search/index.php?direct=true" +
-            "&scope=site&site=eds-live&authtype=ip,guest&custid=s5308004&groupid=main&profile=eds" +
-            "&ailc=y&facet=eBooks&bquery=" + encodeURIComponent(document.forms[search_form]["book_search_input"].value);
+  if (book_filter == "online") {
+    query += "&mfacet=tlevel,include,online_resources,2";
+  }
+  else if (book_filter == "library") {
+    query += "&mfacet=tlevel,include,available_on_shelf,1";
   }
 
-  if (all_books_filter) {
-    query = "https://login.proxy.lib.fsu.edu/login?url=" +
-            "http://widgets.ebscohost.com/prod/search/index.php?direct=true" +
-            "&scope=site&site=eds-live&authtype=ip,guest&custid=s5308004&groupid=main&profile=eds" +
-            "&ailc=y&facet=eBooks,Books&bquery=" + encodeURIComponent(document.forms[search_form]["book_search_input"].value);
-  }
+  query += "&lang=en&offset=0";
 
   window.location = query;
   return false;
